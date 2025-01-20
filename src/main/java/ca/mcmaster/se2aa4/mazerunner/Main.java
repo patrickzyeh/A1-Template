a9c4b5328e716492bd02cd83e26b25b96d2d34b1
patrickzyeh <patrickzyeh@gmail.com>
2025-01-20 13:18:34 -0500

    Added Apache CLI flag parsing

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,20 +1,41 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
+    private static String fileName;
 
     public static void main(String[] args) {
+
+        // CLI Parsing
+
+        System.out.println("**** Reading Command-Line Arguements");
+
+        Options options = new Options();
+        options.addOption("i", true, "Flag to indicate to program that a maze filepath will be provided"); // -i flag
+        options.addOption("p", true, "Flag to indicate to program to validate a path sequence"); // -p flag
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            CommandLine cmd = parser.parse(options, args); // Parses the command line arguements accordingly
+            fileName = cmd.getOptionValue("i");
+            System.out.println("****** Parsed file name: " + fileName);
+        } catch (ParseException e) {
+            System.err.println("An error has occurred");
+        }
+
+        // Reading parsed file
+
         System.out.println("** Starting Maze Runner");
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            System.out.println("**** Reading the maze from file " + fileName);
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -26,8 +47,8 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+        } catch (Exception e) {
+            System.err.println("/!\\ An error has occured while reading from file /!\\");
         }
         System.out.println("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
